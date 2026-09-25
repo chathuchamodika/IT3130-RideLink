c5a4cbf7d5d240355cb71f678fafc32ecf1d0f5f
Chamodika <it24103832@my.sliit.lk>
2026-09-25 20:26:12 +0530

    Configure AccountService dependency injection

--- src/main/java/com/ridelink/account_service/service/AccountService.java
+++ src/main/java/com/ridelink/account_service/service/AccountService.java
@@ -1,4 +1,14 @@
 package com.ridelink.account_service.service;
 
+import com.ridelink.account_service.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 }
